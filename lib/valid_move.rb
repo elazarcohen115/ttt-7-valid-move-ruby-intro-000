2ef1f075b8eb79c5821a76ed79541fefbfcf2f4e
elazar cohen <escohen115@gmail.com>
2020-04-07 16:47:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def position_taken? (board, index)
   end
 end
 
-def valid_move? (move)
+def valid_move? (move, position_taken?)
   if !position_taken? && move > -1 && move < 9
     return true
   else
